c624dbe9f8e24fcf7893232678638c54998b2bd9
Ken Okerlund <ken.okerlund@gmail.com>
2023-01-18 19:34:57 -0700

    Assignment completed

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -127,6 +127,12 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
 
 //Code Here
+for(let i=0; i<listOfNumbers.length; i++){
+  if(listOfNumbers[i] % 3 == 0){
+    let number = listOfNumbers[i];
+    console.log(`${number} is divisible by 3`)
+  }
+}
 
 
 ////////// PROBLEM 9 //////////
@@ -139,7 +145,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+for(let i=letters.length; i>=0; i--){
+  console.log(letters[i])
+}
 
 ////////// Advanced Problems //////////
 
@@ -159,14 +167,32 @@ If D: "The student is not doing very well."
 If F: "The student is failing."
 If the letter grade is not one of the above letters, console.log 'Not an eligible grade.'
 */
-
+switch(letterGrade){
+  case  'A':
+    console.log("The student is doing excellently.");
+    break;
+  case 'B':
+      console.log("The student is doing well.");
+      break;
+  case 'C':
+    console.log("The student is doing alright.");
+    break;
+  case 'D':
+    console.log("The student is not doing very well.");
+    break;
+  case 'F':
+    console.log("The student is failing.");
+    break;
+}
 //Code Here
 
 
 
 ////////// PROBLEM 11 //////////
 /* The famous FizzBuzz, Devmountain style!
-  Create a for loop that iterates from 1 to 100. In this for loop, using some conditional logic, if the number your for loop is currently on is divisible by 3, console.log 'Dev'. If the number is divisible by 5, console.log 'mountain'. If the number is divisible by 5 & 3, console.log 'Devmountain'. If the number is not divisible by 5 or 3, console.log the number itself. Hint: Look up the modulo operator.
+  Create a for loop that iterates from 1 to 100. In this for loop, using some conditional logic, if the number your for loop is currently on is divisible by 3, 
+  console.log 'Dev'. If the number is divisible by 5, console.log 'mountain'. If the number is divisible by 5 & 3, console.log 'Devmountain'. 
+  If the number is not divisible by 5 or 3, console.log the number itself. Hint: Look up the modulo operator.
 
   Your output should look like:
   1
@@ -188,4 +214,15 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
   ...
 */
 
+for(let i=0; i<100; i++){
+  if(i % 3 == 0 && i % 5 ==0){
+    console.log("Devmountain");
+  } else if(i % 3 == 0){
+    console.log("Dev");
+  } else if(i % 5 == 0){
+    console.log("mountain");
+  }else{
+    console.log(i);
+  }
+}
 //Code Here
